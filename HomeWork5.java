8d8cfcbe6c06773a09a91ffc6709e7bb3226b6fc
Seven2seven <52880440+Seven2seven@users.noreply.github.com>
2020-05-02 15:09:51 +0600

    Create HomeWork5.java

---
+++ HomeWork5.java
@@ -0,0 +1,34 @@
+/*
+Mustafa Alibek
+HomeWork5
+Create abstract method in class Employee overall_income() which should return overall income.
+And then in each extended (from Employee) class calculate overall income and then print out.
+ */
+
+abstract class Employee{
+    abstract int overall_income();
+}
+
+class S1 extends Employee{
+    public int salary = 250000;
+
+    int overall_income() {
+        return salary;
+    }
+}
+
+class S2 extends Employee {
+    public int salary = 60000;
+
+    int overall_income() {
+        return salary;
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        S1 a = new S1();
+        S2 b = new S2();
+        System.out.print("Overall income: "+(a.overall_income() + b.overall_income()));
+    }
+}
